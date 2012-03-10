2c3bb18ecaf5fef7011abc0c8288ad3c73b5c064
Thorsten Lorenz <thlorenz@gmx.de>
2012-03-10 18:43:33 -0500

    added create link script

---
+++ scripts/create-links.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+ln -s ~/dotfiles/bashrc      ~/.bashrc
+ln -s ~/dotfiles/bashprofile ~/.profile
+ln -s ~/dotfiles/vimrc       ~/.vimrc
+ln -s ~/dotfiles/irbrc       ~/.irbrc
+ln -s ~/dotfiles/vim         ~/.vim
+ln -s ~/dotfiles/ctags       ~/.ctags
