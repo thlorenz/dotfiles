1347547af354f3ae4915633bbf800a95a067876a
Thorsten Lorenz <tlorenz@lab49.com>
2013-01-28 11:56:07 -0500

    adding tap snippet

---
+++ vim/bundle/snipmate-mine/snippets/javascript/tap.js
@@ -0,0 +1,9 @@
+/*jshint asi: true */
+'use strict';
+
+var testutil = require('test-util')
+var sinon = require('sinon')
+var proxyquire = require('proxyquire')
+var test = require('tap').test
+
+{$1}
