3746d462c8087ea41a7b90de102ec1c58aa19125
Thorsten Lorenz <thlorenz@gmx.de>
2013-06-01 23:39:08 -0400

    adding ngen functions

---
+++ functions.sh
@@ -0,0 +1,13 @@
+. ~/dotfiles/functions-ngen.sh
+
+function st
+{
+  local title=$1
+  echo -n -e "\033]0;$title\007"
+}
+
+function qt 
+{
+  local title="$(basename `pwd`)"
+  echo -n -e "\033]0;$title\007"
+}
