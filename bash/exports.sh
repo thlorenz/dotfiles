12d7a4eb1c9269d36c62442c9713e16ba4b40847
Thorsten Lorenz <thlorenz@gmx.de>
2013-06-08 16:45:05 -0400

    adding exports

---
+++ bash/exports.sh
@@ -0,0 +1,8 @@
+# Github
+
+export GITHUB_TOKEN=$(git config --get github.token)
+export GITHUB_USER=$(git config --get github.user)
+export GIT_COMMITTER_NAME=${GITHUB_USER:-$(git config --get user.name)}
+export GIT_COMMITTER_EMAIL=$(git config --get user.email)
+export GIT_AUTHOR_NAME=${GITHUB_USER:-$(git config --get user.name)}
+export GIT_AUTHOR_EMAIL=$(git config --get user.email)
