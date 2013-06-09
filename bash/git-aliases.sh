dac78a4994fe6d498d0b5c8fcbe36aa5216b4555
Thorsten Lorenz <thlorenz@gmx.de>
2013-06-09 16:08:19 -0400

    adjusting sh git aliases to by more in sync with git aliases

--- bash/git-aliases.sh
+++ bash/git-aliases.sh
@@ -1,15 +1,21 @@
 # Git
 alias ga='git add'
 alias gp='git push'
-alias gl='git log'
+alias gl='git lg'
 alias gs='git status --ignore-submodules -s'
-alias gd='git diff'
-alias gdc='git diff --cached'
-alias gm='git commit -m'
-alias gma='git commit -am'
 alias gb='git branch'
 alias gc='git checkout'
+
+alias gd='git diff'
+alias gdc='git diff --cached'
+
+alias gm='git commit -m'
+alias gam='git commit -am'
+
 alias gra='git remote add'
 alias grr='git remote rm'
-alias gpu='git pull --rebase'
+
+alias gpu='git pull'
+alias gpur='git pull --rebase'
+
 alias gcl='git clone'
