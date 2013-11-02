ad10b58a823611d3d7868de9f3369d6214e95f08
Thorsten Lorenz <thlorenz@gmx.de>
2013-11-02 14:02:24 -0400

    completing create-links script

--- scripts/create-links.sh
+++ scripts/create-links.sh
@@ -2,8 +2,14 @@
 
 ln -s ~/dotfiles/bashrc      ~/.bashrc
 ln -s ~/dotfiles/bashprofile ~/.profile
+ln -s ~/dotfiles/inputrc     ~/.inputrc
 ln -s ~/dotfiles/vimrc       ~/.vimrc
 ln -s ~/dotfiles/irbrc       ~/.irbrc
 ln -s ~/dotfiles/vim         ~/.vim
 ln -s ~/dotfiles/ctags       ~/.ctags
-ln -s ~/dotfiles/irssi       ~/.irssi
+ln -s ~/dotfiles/gitconfig   ~/.gitconfig
+ln -s ~/dotfiles/tigrc       ~/.tigrc
+ln -s ~/dotfiles/cardinalrc  ~/.cardinalrc
+
+ln -s ~/dotfiles/config/nstart  ~/.config/nstart
+ln -s ~/dotfiles/config/pkginit ~/.config/pkginit
