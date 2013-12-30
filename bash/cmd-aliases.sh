c781b7a3f588871311d54258b39238652a7ba7e5
Thorsten Lorenz <thlorenz@gmx.de>
2013-12-30 11:04:24 -0500

    resolving cherry-pick conflict

---
+++ bash/cmd-aliases.sh
@@ -0,0 +1,18 @@
+# enable color support of ls and also add color related aliases
+if [ -x /usr/bin/dircolors ]; then
+  test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+  
+  alias ls='ls --color=auto'
+  alias grep='grep --color=auto'
+  alias fgrep='fgrep --color=auto'
+  alias egrep='egrep --color=auto'
+fi
+
+# Aliases
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+
+alias c='pygmentize -O style=monokai -f console256 -g'
+alias tree='tree -I node_modules'
+alias info='info --vi-keys' 
