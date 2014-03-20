7dbe1b69f1df6b672960bbe0899836f9aac40f30
Thorsten Lorenz <thlorenz@gmx.de>
2014-03-20 15:55:33 -0400

    forcing link creation

--- scripts/create-links.sh
+++ scripts/create-links.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env sh
 
-ln -s ~/dotfiles/bashrc      ~/.bashrc
-ln -s ~/dotfiles/bashprofile ~/.profile
-ln -s ~/dotfiles/inputrc     ~/.inputrc
-ln -s ~/dotfiles/vimrc       ~/.vimrc
-ln -s ~/dotfiles/irbrc       ~/.irbrc
-ln -s ~/dotfiles/vim         ~/.vim
-ln -s ~/dotfiles/ctags       ~/.ctags
-ln -s ~/dotfiles/gitconfig   ~/.gitconfig
-ln -s ~/dotfiles/tigrc       ~/.tigrc
-ln -s ~/dotfiles/cardinalrc  ~/.cardinalrc
-ln -s ~/dotfiles/agignore    ~/.agignore
+ln -F -s ~/dotfiles/bashrc      ~/.bashrc
+ln -F -s ~/dotfiles/bashprofile ~/.profile
+ln -F -s ~/dotfiles/inputrc     ~/.inputrc
+ln -F -s ~/dotfiles/vimrc       ~/.vimrc
+ln -F -s ~/dotfiles/irbrc       ~/.irbrc
+ln -F -s ~/dotfiles/ctags       ~/.ctags
+ln -F -s ~/dotfiles/gitconfig   ~/.gitconfig
+ln -F -s ~/dotfiles/tigrc       ~/.tigrc
+ln -F -s ~/dotfiles/cardinalrc  ~/.cardinalrc
+ln -F -s ~/dotfiles/agignore    ~/.agignore
 
+ln -s ~/dotfiles/vim            ~/.vim
 ln -s ~/dotfiles/config/nstart  ~/.config/nstart
 ln -s ~/dotfiles/config/pkginit ~/.config/pkginit
