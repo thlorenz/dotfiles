c87786011f7ce7d987f3ed6eb447fafdb543aebf
Thorsten Lorenz <thlorenz@gmx.de>
2014-04-02 10:47:45 -0400

    fixing cnpm registry

--- bash/node-aliases.sh
+++ bash/node-aliases.sh
@@ -1,3 +1,3 @@
 alias lnpm='npm --no-registry'
-alias cnpm='npm --reg https://condenast.npmjitsu.co'
+alias cnpm='npm --reg https://condenast.registry.nodejitsu.com'
 alias enpm='npm --registry http://registry.npmjs.eu'
