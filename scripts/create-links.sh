cb18b83a42f307ef798e0dfbdaaa2b7b5288cc5b
Thorsten Lorenz <thlorenz@gmx.de>
2014-08-09 17:09:53 -0400

    lldb init config

--- scripts/create-links.sh
+++ scripts/create-links.sh
@@ -5,17 +5,18 @@ unamestr=`uname`
 
 rm -f ~/.bashrc ~/.bashprofile
 
-ln -s ~/dotfiles/bashrc      ~/.bashrc
-ln -s ~/dotfiles/bashprofile ~/.profile
-ln -s ~/dotfiles/inputrc     ~/.inputrc
-ln -s ~/dotfiles/vimrc       ~/.vimrc
-ln -s ~/dotfiles/irbrc       ~/.irbrc
-ln -s ~/dotfiles/ctags       ~/.ctags
-ln -s ~/dotfiles/gitconfig   ~/.gitconfig
-ln -s ~/dotfiles/tigrc       ~/.tigrc
-ln -s ~/dotfiles/cardinalrc  ~/.cardinalrc
-ln -s ~/dotfiles/agignore    ~/.agignore
-ln -s ~/dotfiles/tmux/tmux.conf  ~/.tmux.conf 
+ln -s ~/dotfiles/bashrc             ~/.bashrc
+ln -s ~/dotfiles/bashprofile        ~/.profile
+ln -s ~/dotfiles/inputrc            ~/.inputrc
+ln -s ~/dotfiles/vimrc              ~/.vimrc
+ln -s ~/dotfiles/irbrc              ~/.irbrc
+ln -s ~/dotfiles/ctags              ~/.ctags
+ln -s ~/dotfiles/gitconfig          ~/.gitconfig
+ln -s ~/dotfiles/tigrc              ~/.tigrc
+ln -s ~/dotfiles/cardinalrc         ~/.cardinalrc
+ln -s ~/dotfiles/agignore           ~/.agignore
+ln -s ~/dotfiles/tmux/tmux.conf     ~/.tmux.conf 
+ln -s ~/dotfiles/lldb/lldbinit-lldb ~/.lldbinit-lldb 
 
 ln -s ~/dotfiles/vim         ~/.vim
 
