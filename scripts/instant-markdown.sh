dfe3b344a6b7a8d5d15398a4138adb0b05c81eea
Thorsten Lorenz <thlorenz@gmx.de>
2014-10-23 13:40:35 -0400

    vim: adding instant markdown module and associated install script

---
+++ scripts/instant-markdown.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+## Assumimg we have Ruby 2.0 (https://github.com/suan/vim-instant-markdown#installation)
+gem install redcarpet pygments.rb --no-ri --no-doc
+npm install -g instant-markdown-d
