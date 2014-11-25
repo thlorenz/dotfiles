1f4ddbe1df5d6288588d9b7a628de544ab6cc08c
Thorsten Lorenz <thlorenz@gmx.de>
2014-11-25 13:45:50 -0500

    fixing copy fonts script

--- scripts/copy-fonts.sh
+++ scripts/copy-fonts.sh
@@ -5,6 +5,6 @@ unamestr=`uname`
 
 if [ "$unamestr" == "Linux" ]; then
   cp ~/dotfiles/fonts/* /usr/share/fonts
-elif ["$unamestr" == "Darwin" ]; then
+elif [ "$unamestr" == "Darwin" ]; then
   cp ~/dotfiles/fonts/* ~/Library/Fonts/ 
 fi
