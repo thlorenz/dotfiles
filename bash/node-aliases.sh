39eaeeff4742ada8db5f242f91f9b8098b432f8e
Thorsten Lorenz <thlorenz@gmx.de>
2015-10-08 21:32:31 +0200

    improfing lnpm alias

--- bash/node-aliases.sh
+++ bash/node-aliases.sh
@@ -1,3 +1,3 @@
-alias lnpm='npm --no-registry'
+alias lnpm='npm --cache-min=Infinity'
 alias cnpm='npm --reg https://condenast.registry.nodejitsu.com'
 alias enpm='npm --registry http://registry.npmjs.eu'
