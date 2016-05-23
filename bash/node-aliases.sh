17de004cb96a30d7023c6fe225e140c556b9e1b6
Thorsten Lorenz <thlorenz@gmx.de>
2016-05-23 14:41:07 -0700

    adding node-gyp generate xcode project alias

--- bash/node-aliases.sh
+++ bash/node-aliases.sh
@@ -1,3 +1,4 @@
 alias lnpm='npm --cache-min=Infinity'
 alias cnpm='npm --reg https://condenast.registry.nodejitsu.com'
 alias enpm='npm --registry http://registry.npmjs.eu'
+alias gypx='node-gyp configure -- -f xcode'
