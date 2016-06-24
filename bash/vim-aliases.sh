4ce3c2e3ceade71b91f28ed8845fb8b0ed315849
Thorsten Lorenz <thlorenz@gmx.de>
2016-06-24 10:50:23 -0500

    bash: adding quick way to switch between standard and standart

--- bash/vim-aliases.sh
+++ bash/vim-aliases.sh
@@ -1,3 +1,8 @@
 # Open all modified an new files
 alias vmno='vim -O `git status --porcelain | awk '"'"'{ print $2 }'"'"'`'
 alias vmn='vim `git status --porcelain | awk '"'"'{ print $2 }'"'"'`'
+
+# Switch standard used
+alias std='(cd /Users/thlorenz/npm-global/bin/ && rm -f standard && ln -s standardd standard)'
+alias stt='(cd /Users/thlorenz/npm-global/bin/ && rm -f standard && ln -s standart  standard)'
+
