53b833d2dea81ebfdb5fef614e2a4c347f002957
Thorsten Lorenz <thlorenz@gmx.de>
2016-10-13 16:36:19 -0500

    bash: adding compress pdf via ghostscript function

--- bash/functions/util.sh
+++ bash/functions/util.sh
@@ -26,3 +26,7 @@ padd() {
       PATH="$p:$PATH"
   fi
 }
+
+compresspdf() {
+  \gs -sDEVICE=pdfwrite -dNOPAUSE -dQUIET -dBATCH -dPDFSETTINGS=/${3:-"screen"} -dCompatibilityLevel=1.4 -sOutputFile=$2 $1
+}
