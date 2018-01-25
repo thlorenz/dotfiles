60901fd4ed0d551cd59885f589224a416daf134c
Thorsten Lorenz <thlorenz@gmx.de>
2018-01-25 14:08:10 -0500

    git: improving git aliases

--- bash/git-aliases.sh
+++ bash/git-aliases.sh
@@ -2,19 +2,29 @@
 alias ga='git add'
 alias gp='git push'
 alias gl='git lg'
+
+# outputs logs sorted by committer date
+alias gld='git ld | sort -r | more -R'
 alias gs='git status --ignore-submodules -s'
-alias gb='git branch'
+alias gb='git branch -v'
 alias gc='git checkout'
 
+# diffing
 alias gd='git diff'
+alias gt='git difftool'
 alias gdc='git diff --cached'
+alias gtc='git diff --cached'
 
+# committing
 alias gm='git commit -m'
 alias gam='git commit -am'
+alias gamd='git commit --amend --no-edit'
 
+# remotes
 alias gra='git remote add'
 alias grr='git remote rm'
 
+# pulling and cloning
 alias gpu='git pull'
 alias gpur='git pull --rebase'
 
