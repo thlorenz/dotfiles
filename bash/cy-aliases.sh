a39b33be469715681335f1d62ef8be1f19e0101c
Thorsten Lorenz <thlorenz@gmx.de>
2020-02-29 10:08:30 -0500

    cy: adding jest/cyr aliases

--- bash/cy-aliases.sh
+++ bash/cy-aliases.sh
@@ -1,1 +1,13 @@
 alias tr='yarn tsc:tr && yarn lint:tr && yarn test'
+
+function jtc () {
+  node --require '/Users/thlorenz/Library/Application Support/JetBrains/Toolbox/apps/IDEA-U/ch-0/193.6494.35/IntelliJ IDEA.app/Contents/plugins/JavaScriptLanguage/helpers/jest-intellij/lib/jest-intellij-stdin-fix.js' \
+    '/Users/thlorenz/dev/cy/cypress-node/cypress-node/node_modules/jest/bin/jest.js' --colors \
+    --reporters '/Users/thlorenz/Library/Application Support/JetBrains/Toolbox/apps/IDEA-U/ch-0/193.6494.35/IntelliJ IDEA.app/Contents/plugins/JavaScriptLanguage/helpers/jest-intellij/lib/jest-intellij-reporter.js' \
+    --verbose --runTestsByPath $1 --testNamePattern=$2
+}
+
+function ctc () {
+  node '/Users/thlorenz/dev/cy/cypress-node/cypress-node/packages/cyr/bin/cyr.js' $1 \
+    --reporters teamcity --verbose --testNamePattern=$2
+}
