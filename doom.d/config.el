f8c99c5955f46f1bfc61e65f20fde4f84268e6c7
Thorsten Lorenz <thlorenz@gmx.de>
2020-04-28 09:13:18 -0600

    emacs: simplifying tmux plugin

--- doom.d/config.el
+++ doom.d/config.el
@@ -64,18 +64,6 @@
 (use-package! tmux-pane
   :config
   (tmux-pane-mode))
-  (map! :leader
-        (:prefix ("v" . "tmux pane")
-          :desc "Open vpane" :nv "o" #'tmux-pane-open-vertical
-          :desc "Open hpane" :nv "h" #'tmux-pane-open-horizontal
-          :desc "Open hpane" :nv "s" #'tmux-pane-open-horizontal
-          :desc "Open vpane" :nv "v" #'tmux-pane-open-vertical
-          :desc "Close pane" :nv "c" #'tmux-pane-close
-          :desc "Rerun last command" :nv "r" #'tmux-pane-rerun))
-  (map! :leader
-        (:prefix "t"
-          :desc "vpane" :nv "v" #'tmux-pane-toggle-vertical
-          :desc "hpane" :nv "h" #'tmux-pane-toggle-horizontal)))
 
 ;; no backup/lock files
 (setq make-backup-files nil)
