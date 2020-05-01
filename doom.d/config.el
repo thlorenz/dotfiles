cd4fe080403494ac95bfa76b240f7b8de0f42b17
Thorsten Lorenz <thlorenz@gmx.de>
2020-05-01 14:19:22 -0600

    emacs: make system clipboard work

--- doom.d/config.el
+++ doom.d/config.el
@@ -93,3 +93,16 @@
 
 ;; org mode
 (eval-after-load 'org '(require 'ox-gfm nil t))
+
+;; clipboard
+(defun copy-from-osx ()
+  (shell-command-to-string "pbpaste"))
+
+(defun paste-to-osx (text &optional push)
+  (let ((process-connection-type nil))
+    (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+      (process-send-string proc text)
+      (process-send-eof proc))))
+
+(setq interprogram-cut-function 'paste-to-osx)
+(setq interprogram-paste-function 'copy-from-osx)
