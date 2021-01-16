ce8d928fd9435d7a29706429eebdd65f7ed62a66
Thorsten Lorenz <thlorenz@gmx.de>
2021-01-16 09:33:05 -0500

    bash: rust aliases

--- bash/rust-aliases.sh
+++ bash/rust-aliases.sh
@@ -1,8 +1,22 @@
+# Disabled sccache for now since it doesn't work when building C++
+# Instead adding aliases below to build with sccache explixitly
+# `cargo install sccache`
+# export RUSTC_WRAPPER=sccache
+
+alias cg-build-caching='RUSTC_WRAPPER=sccache cargo build'
 alias cg-watch-build='cargo-watch -s "clear; cargo build"'
+alias cg-watch-build-caching='cargo-watch -s "clear; RUSTC_WRAPPER=sccache cargo build"'
+alias cg-watch-test-noc='cargo-watch -s "clear; cargo test -- --nocapture"'
 
 function cg-watch-bin() {
   local bin="$1"
   local command="clear; cargo run --bin ${bin}"
+  cargo-watch -s "$command"
+}
+
+function cg-watch-example() {
+  local bin="$1"
+  local command="clear; cargo run --example ${bin}"
   cargo-watch -s "$command"
 }
 
