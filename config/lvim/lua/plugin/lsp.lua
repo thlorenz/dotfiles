abbe9cd65014e40722725ed166cd5d03d997f66e
Thorsten Lorenz <thlorenz@gmx.de>
2022-06-20 00:13:51 -0600

    lvim: lsp codelens refresh before run

---
+++ config/lvim/lua/plugin/lsp.lua
@@ -0,0 +1,32 @@
+lvim.builtin.which_key.mappings["l"] = {
+  name = "LSP",
+  a = { "<cmd>lua vim.lsp.buf.code_action()<cr>", "Code Action" },
+  d = { "<cmd>Telescope diagnostics bufnr=0 theme=get_ivy<cr>", "Buffer Diagnostics" },
+  w = { "<cmd>Telescope diagnostics<cr>", "Diagnostics" },
+  f = { require("lvim.lsp.utils").format, "Format" },
+  i = { "<cmd>LspInfo<cr>", "Info" },
+  I = { "<cmd>LspInstallInfo<cr>", "Installer Info" },
+  j = {
+    vim.diagnostic.goto_next,
+    "Next Diagnostic",
+  },
+  k = {
+    vim.diagnostic.goto_prev,
+    "Prev Diagnostic",
+  },
+  l = { function() vim.lsp.codelens.refresh(); vim.lsp.codelens.run() end, "CodeLens Action" },
+  p = {
+    name = "Peek",
+    d = { "<cmd>lua require('lvim.lsp.peek').Peek('definition')<cr>", "Definition" },
+    t = { "<cmd>lua require('lvim.lsp.peek').Peek('typeDefinition')<cr>", "Type Definition" },
+    i = { "<cmd>lua require('lvim.lsp.peek').Peek('implementation')<cr>", "Implementation" },
+  },
+  q = { vim.diagnostic.setloclist, "Quickfix" },
+  r = { vim.lsp.buf.rename, "Rename" },
+  s = { "<cmd>Telescope lsp_document_symbols<cr>", "Document Symbols" },
+  S = {
+    "<cmd>Telescope lsp_dynamic_workspace_symbols<cr>",
+    "Workspace Symbols",
+  },
+  e = { "<cmd>Telescope quickfix<cr>", "Telescope Quickfix" },
+}
