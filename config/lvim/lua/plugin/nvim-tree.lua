88c507861782d5cf6bf5ec5fc8c7ad0429f6975f
Thorsten Lorenz <thlorenz@gmx.de>
2022-06-25 09:10:08 -0600

    lvim: nvim tree no longer follows dirs

--- config/lvim/lua/plugin/nvim-tree.lua
+++ config/lvim/lua/plugin/nvim-tree.lua
@@ -3,5 +3,6 @@ lvim.builtin.which_key.mappings["e"] = nil
 lvim.builtin.which_key.mappings["n"] = { "<cmd>NvimTreeToggle<CR>", "Explorer" }
 
 -- config
-lvim.builtin.nvimtree.setup.view.side = "left"
-lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
+lvim.builtin.nvimtree.setup.view.side                      = "left"
+lvim.builtin.nvimtree.setup.renderer.icons.show.git        = true
+lvim.builtin.nvimtree.setup.update_focused_file.update_cwd = false
