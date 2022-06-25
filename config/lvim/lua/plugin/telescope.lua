c029a5a33c42d8fe7f5b028b55eadd6749e05474
Thorsten Lorenz <thlorenz@gmx.de>
2022-06-25 09:11:09 -0600

    lvim: telescope improve emoji setup

--- config/lvim/lua/plugin/telescope.lua
+++ config/lvim/lua/plugin/telescope.lua
@@ -71,11 +71,14 @@ lvim.builtin.telescope['extensions'] = {
   },
   ["emoji"] = {
     action = function(emoji)
+      -- argument emoji is a table.
+      -- {name="", value="", cagegory="", description=""}
+
       vim.fn.setreg("*", emoji.value)
       print([[Press p or "*p to paste this emoji]] .. emoji.value)
 
       -- insert emoji when picked
-      vim.api.nvim_put({ emoji.value }, 'c', false, true)
+      -- vim.api.nvim_put({ emoji.value }, 'c', false, true)
     end,
   }
 }
