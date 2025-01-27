084213375960071d85a41cd25ef043c50e9a3285
Thorsten Lorenz <thlorenz@gmx.de>
2025-01-27 09:46:18 -0700

    init: tweak brew to current needs

--- scripts/brew-installs.sh
+++ scripts/brew-installs.sh
@@ -8,33 +8,20 @@ brew tap phinze/cask
 brew install brew-cask
 
 # dev/terminal tools
-brew install macvim
-brew install the_silver_searcher
 brew install tree
 brew install htop-osx
 brew install iftop
-# brew install tig
 brew install tmux
 brew install wget
 brew install autojump
 brew install bash
-# brew install chrome-cli
-
 
 # Debugger/Compiler support
 brew install autoconf
 brew install cmake
-# brew install ctags
-# brew install homebrew/dupes/gdb
-# brew install swig
-
-# devops
-brew install ansible
-# brew install s3cmd
-# brew install ssh-copy-id
 
 brew install nmap
-brew install openssl
+# brew install openssl
 brew install pkg-config
 
 # change brightness of monitors programatically
@@ -46,16 +33,9 @@ brew install switchaudio-osx
 # cast keys
 brew cask install keycastr
 
-# testing
-# brew install wrk
-# brew install siege
-
-## Some like chrome seemed to have permissions problems when installed this way
-# brew cask install google-chrome
-# brew cask install bettertouchtool
-# brew cask install iterm2
-# brew cask install limechat
-# brew cask install transmission
+brew install bettertouchtool
+brew install iterm2
+brew install transmission
 brew install vlc
 
 ###########################
@@ -82,41 +62,13 @@ brew install duf
 ### https://clementtsang.github.io/bottom/configuration/config-file/flags/
 brew tap clementtsang/bottom && brew install bottom
 
-# emacs
-# brew tap d12frosted/emacs-plus
-# brew install emacs-plus
-# rm -rf /Applications/Emacs.app
-# ln -s /usr/local/opt/emacs-plus/Emacs.app /Applications/Emacs.app
-
-# mac emacs (needed to work well with tools like yabai)
-# brew tap railwaycat/emacsmacport
-# rm -rf /Applications/Emacs.app
-# brew cask install emacs-mac
-# brew untap railwaycat/emacsmacport
-
-# pdflatex to convert emacs org files to pdf
-# brew cask install mactex
-
 # yabai and related keyboard hooks
-# this tool proved too unstable for now
-# brew install koekeishiya/formulae/yabai
-# sudo yabai --install-sa
-# brew services start yabai
-# brew install koekeishiya/formulae/skhd
-# brew services start skhd
-# killall Dock
-
-# To have launchd start d12frosted/emacs-plus/emacs-plus now and restart at login:
-#   brew services start d12frosted/emacs-plus/emacs-plus
-
-## emacs/doom deps
-# https://github.com/hlissner/doom-emacs/blob/develop/docs/getting_started.org#with-homebrew
-brew install ripgrep
-brew install coreutils fd
+brew install koekeishiya/formulae/yabai
+sudo yabai --install-sa
+brew install koekeishiya/formulae/skhd
 
 # productivity
 brew install terminal-notifier
-
 
 brew link
 brew doctor
